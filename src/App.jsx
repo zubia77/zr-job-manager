8057fbeffcb4d9608d948ec4d5995e2af4bcb371
Zubia <rashzubia@gmail.com>
2022-07-14 09:04:04 +0200

    site tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,112 @@
-import { useState, useEffect } from 'react';
-import './App.scss';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import "./App.scss";
+import axios from "axios";
 
-const backend_base_url = 'http://localhost:3046';
+const backend_base_url = "http://localhost:3046";
 
 function App() {
-	const [jobSources, setJobSources] = useState([]);
-	const [currentUser, setCurrentUser] = useState({});
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-	const [message, setMessage] = useState('');
+    const [jobSources, setJobSources] = useState([]);
+    const [currentUser, setCurrentUser] = useState({});
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
 
-	const userIsLoggedIn = () => {
-		return Object.keys(currentUser).length > 0;
-	};
+    const userIsLoggedIn = () => {
+        return Object.keys(currentUser).length > 0;
+    };
 
-	const getJobSources = () => {
-		(async () => {
-			setJobSources(
-				(await axios.get(backend_base_url + '/job-sources')).data
-			);
-		})();
-	};
+    const getJobSources = () => {
+        (async () => {
+            setJobSources(
+                (await axios.get(backend_base_url + "/job-sources")).data
+            );
+        })();
+    };
 
-	useEffect(() => {
-		if (userIsLoggedIn()) {
-			getJobSources();
-		}
-	}, []);
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(backend_base_url + "/maintain-login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    authorization: "Bearer " + localStorage.getItem("token"),
+                },
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setCurrentUser(data.user);
+                getJobSources();
+            } else {
+                setCurrentUser({});
+            }
+        })();
+    }, []);
 
-	const handleLoginButton = async (e) => {
-		e.preventDefault();
-		const response = await fetch(backend_base_url + '/login', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ username, password }),
-		});
-		setUsername('');
-		setPassword('');
-		if (response.ok) {
-			const data = await response.json();
-			getJobSources();
+    const handleLoginButton = async (e) => {
+        e.preventDefault();
+        const response = await fetch(backend_base_url + "/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password }),
+        });
+        setUsername("");
+        setPassword("");
+        if (response.ok) {
+            const data = await response.json();
+            getJobSources();
             console.log(data);
-			setCurrentUser(data.user);
-		} else {
-			setMessage('bad login');
-		}
-	};
+            setCurrentUser(data.user);
+            localStorage.setItem("token", data.token);
+        } else {
+            setMessage("bad login");
+        }
+    };
+    const handleLogoutButton = () => {
+        localStorage.setItem('token', '');
+        setCurrentUser({});
+    };
 
-	return (
-		<div className="App">
-			<h1>Zubi Job Manager</h1>
-			{userIsLoggedIn() ? (
-				<>
-					<p>There are {jobSources.length} job sources:</p>
-					<ul>
-						{jobSources.map((jobSource, i) => {
-							return <li key={i}>{jobSource.name}</li>;
-						})}
-					</ul>
-				</>
-			) : (
-				<form className="login" onSubmit={handleLoginButton}>
-					<div className="row">
-						username:{' '}
-						<input
-							onChange={(e) => setUsername(e.target.value)}
-							value={username}
-							type="text"
-						/>
-					</div>
-					<div className="row">
-						password:{' '}
-						<input
-							onChange={(e) => setPassword(e.target.value)}
-							value={password}
-							type="password"
-						/>
-					</div>
-					<div className="row">
-						<button>Login</button>
-					</div>
-					<div className="row">{message}</div>
-				</form>
-			)}
-		</div>
-	);
+    return (
+        <div className="App">
+            <h1>Zubi Job Manager</h1>
+            {userIsLoggedIn() ? (
+                <>
+                    <p>There are {jobSources.length} job sources:</p>
+                    <ul>
+                        {jobSources.map((jobSource, i) => {
+                            return <li key={i}>{jobSource.name}</li>;
+                        })}
+                    </ul>
+                    <button className="logout" onClick={handleLogoutButton}>
+                        Logout
+                    </button>
+                </>
+            ) : (
+                <form className="login" onSubmit={handleLoginButton}>
+                    <div className="row">
+                        username:{" "}
+                        <input
+                            onChange={(e) => setUsername(e.target.value)}
+                            value={username}
+                            type="text"
+                        />
+                    </div>
+                    <div className="row">
+                        password:{" "}
+                        <input
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            type="password"
+                        />
+                    </div>
+                    <div className="row">
+                        <button>Login</button>
+                    </div>
+                    <div className="row">{message}</div>
+                </form>
+            )}
+        </div>
+    );
 }
 
 export default App;
